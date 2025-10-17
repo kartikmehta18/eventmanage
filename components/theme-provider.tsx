55137b137e3bdecbeb6605e38cfa63cf548a6624
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-10-17 12:28:51 +0300

    first commit

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,8 +3,6 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-// Derive the props type from the NextThemesProvider to avoid importing
-// internal/deep paths that may not exist in the installed package.
 type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
